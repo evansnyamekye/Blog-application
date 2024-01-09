045f182045a49d2b2b9f1d541b84495f9e86318d
Evans K. Nyamekye <nyamekye2131@gmail.com>
2024-01-09 05:24:00 +0000

    Update: Add schema.rb and changed type to text from string

--- db/migrate/20240108184133_create_users.rb
+++ db/migrate/20240108184133_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :name
       t.string :photo
       t.text :bio
-      t.string :posts_counter
+      t.string :posts_counter, default: 0
      
       t.timestamps
     end
