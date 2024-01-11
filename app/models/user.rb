03258f6f92b511858b91868b84dbe657f45edb04
Evans K. Nyamekye <nyamekye2131@gmail.com>
2024-01-11 14:59:07 +0000

    add validation into user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,18 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def three_recent_posts
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(3)
+  end
+
+  def recent_likes
+    likes.order(created_at: :desc).limit(3)
   end
 end
