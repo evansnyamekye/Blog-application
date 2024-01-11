8be4489f9c80d08f6a670575aa65d7641b0cc572
Evans K. Nyamekye <nyamekye2131@gmail.com>
2024-01-11 14:57:12 +0000

    add validation into post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,19 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
   after_save :update_posts_counter
 
+  validates :title, presence: true, length: { minimum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer:true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+  
   private
 
   def update_posts_counter
-    author.update(posts_count: author.posts.count)
+    author.update(posts_counter: author.posts.count)
   end
 end
