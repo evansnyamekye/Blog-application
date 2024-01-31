369dc8af1c345458f394532ace3796d21a6235c2
izebiafe <afimiafidelis95@gmail.com>
2024-01-31 15:13:39 +0100

    update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
+  def index
+    @user = User.includes(:posts).find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+  end
+
+  def show
+    @user = User.includes(posts: :comments).find(params[:user_id])
+    @post = Post.find(params[:id])
+    @posts = @user.posts
+  end
+
   load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
